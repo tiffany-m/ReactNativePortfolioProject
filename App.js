f7a9372829ad0f24f6b2d124c279c184503ade53
Tiffany <tiffany_mollica@yahoo.com>
2020-09-10 17:43:38 -0400

    Start

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Main from './components/Main';
+console.disableYellowBox = true;
+
+export default function App() {
+  return (
+      <Main />
+  );
+}
---
+++ components/Main.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Card } from 'react-native-elements';
+
+class Main extends Component {
+    render() {
+        return (
+            <Card>
+                <Text> Test </Text>
+            </Card>
+            );
+    }
+}
+
+export default Main;
